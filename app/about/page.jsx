0f22023296a03adb02b69e76f506e188dfd84428
wayeeux <1525625981@qq.com>
2024-08-18 18:44:39 +0800

    init

---
+++ app/about/page.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+const About = () => {
+    return (
+        <div className="bg-slate-200 h-svh">
+            <div>ABOUT
+            </div>
+            <Link href={'/'}>return</Link>
+        </div>
+    )
+}
+export default About
